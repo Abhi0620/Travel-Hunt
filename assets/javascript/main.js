34cb82162e7f04fdc9c16e45ad9018d4c80acd8b
Abhi0620 <155211332+Abhi0620@users.noreply.github.com>
2026-03-27 14:02:21 +0530

    added assets

---
+++ assets/javascript/main.js
@@ -0,0 +1,21 @@
+let navbarlinks = document.querySelectorAll('#nav-bar a');
+
+function navbarlinksActive() {
+  navbarlinks.forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    let position = window.scrollY + 200;
+
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active');
+    } else {
+      navbarlink.classList.remove('active');
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive);
+document.addEventListener('scroll', navbarlinksActive);
